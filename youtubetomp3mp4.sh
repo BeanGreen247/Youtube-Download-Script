053a7228d1d06967ed440bb120f7e8a0238324f9
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2019-12-16 11:38:35 +0100

    Add files via upload

--- youtubetomp3mp4.sh
+++ youtubetomp3mp4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 format=0
-mkdir ~/Converted_Youtube_To_MP3 
+mkdir ~/Converted_Youtube_To_MP3
 cd ~/Converted_Youtube_To_MP3/
 echo "Name the folder you want to save the song(s) to:"
 read directory
@@ -15,11 +15,11 @@ echo "Enter Download Video URL you want to convert to mp3 : "
 read link
 youtube-dl -i --format best --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" $link
 fi
-if [ $format -eq 2 ] 
+if [ $format -eq 2 ]
 then
 echo "Enter Download Video URL you want to convert to mp4 : "
 read link
 youtube-dl -i -o "%(title)s.%(ext)s" $link
 fi
-echo "Done converted files are saved in $pwd"
+echo "Done converted files are saved in ~/Converted_Youtube_To_MP3/$directory"
 cd
